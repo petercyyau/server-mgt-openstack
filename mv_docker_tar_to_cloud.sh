cfee2fd890b09e6ec559a17679e21166d24c4277
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2024-01-29 15:28:24 +0800

    Update and rename mv_docker_tar_to_cloud.sh to openstack_config_file_openrc.sh

--- mv_docker_tar_to_cloud.sh
+++
@@ -1,7 +0,0 @@
-##########################################################
-# Set CRON
-# 0 3 * * * /home/ubuntu/_scripts/mv2cloud.sh> /home/ubuntu/_backups/$(date +\%Y\%m\%d\%H\%M\%S).rc.log 2>&1
-##########################################################
-#!/bin/bash
-
-rclone move -P /home/ubuntu/_backups/ rc_server_cloud_name:
